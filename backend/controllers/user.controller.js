c2726f76c420cd735e7dbd2a4fc240e62f969143
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-11 18:54:11 -0400

    Initial commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -81,7 +81,7 @@ export const login = async (req, res) => {
         const tokenData = {
             userId: user._id
         }
-        const token = await jwt.sign(tokenData, process.env.SECRET_KEY, { expiresIn: '1d' });
+        const token = await jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: '1d' });
 
         user = {
             _id: user._id,
