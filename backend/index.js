5186ac8f182627d62fbd4a1990fe0331d3f34325
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-11 19:42:42 -0400

    url updated in backed

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://mern-job-rbpe.onrender.com",
   credentials: true,
 };
 
