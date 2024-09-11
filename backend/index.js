b92d6fe0da79c539bf0032323448cff8294423ac
RavisherSinghGill <gill1122ravi@gmail.com>
2024-09-11 19:26:33 -0400

    ready to prod

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
@@ -12,19 +14,20 @@ dotenv.config({});
 
 const app = express();
 
+const __dirname = path.resolve();
+
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -32,9 +35,13 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/frontend/dist/index.html"));
+});
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
